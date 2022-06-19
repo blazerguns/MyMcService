56b3f021ef9f35d141410831337b0ff447fe889f
blazerguns <varun.chandramohan1@cba.com.au>
2022-06-20 05:49:39 +1000

    Set keys correctly

--- publish_result/publish.py
+++ publish_result/publish.py
@@ -7,7 +7,7 @@ def publish():
 
    branch = os.environ.get("BRANCH", "")
    token = os.environ.get("TOKEN", "")
-   print("Branch " + branch + " Token " + token + "Keys " + os.environ.get("BRANCH", "GHKEYS"))
+   print("Branch " + branch + " Token " + token + "Keys " + os.environ.get("GHKEYS", "NOTFOUND"))
    pullid = "1"
    payload = {
       "body": "First Header | Second Header\n------------ | -------------\nContent from cell 1 | Content from cell 2\nContent in the first column | Content in the second column"
