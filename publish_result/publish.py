79b7b6a96185da2a26929d62d0d165c5c7cdd6bb
blazerguns <varun.chandramohan1@cba.com.au>
2022-06-20 05:46:51 +1000

    Set keys correctly

--- publish_result/publish.py
+++ publish_result/publish.py
@@ -7,12 +7,12 @@ def publish():
 
    branch = os.environ.get("BRANCH", "")
    token = os.environ.get("TOKEN", "")
+   print("Branch " + branch + " Token " + token + "Keys " + os.environ.get("BRANCH", "GHKEYS"))
    pullid = "1"
    payload = {
       "body": "First Header | Second Header\n------------ | -------------\nContent from cell 1 | Content from cell 2\nContent in the first column | Content in the second column"
    }
    url = "https://api.github.com/repos/blazerguns/MyMcService/issues/{id}/comments".format(id=pullid)
-   print(url)
    try:
       result = requests.post(url, data=json.dumps(payload),
                      headers={'Content-Type': 'application/json',
