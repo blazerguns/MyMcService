3d243c8ea0f25db58c60a78935222bc7d5b49c92
blazerguns <varun.chandramohan1@cba.com.au>
2022-06-20 05:40:19 +1000

    Set url correctly

--- publish_result/publish.py
+++ publish_result/publish.py
@@ -7,18 +7,18 @@ def publish():
 
    branch = os.environ.get("BRANCH", "")
    token = os.environ.get("TOKEN", "")
-   print("The token is " + token)
    pullid = "1"
    payload = {
       "body": "First Header | Second Header\n------------ | -------------\nContent from cell 1 | Content from cell 2\nContent in the first column | Content in the second column"
    }
+   url = "https://api.github.com/repos/blazerguns/MyMcService/issues/{id}/comments".format(id=pullid)
+   print(url)
    try:
-      result = requests.post(f"https://api.github.com/repos/blazerguns/MyMcService/issues/{id}/comments".format(id=pullid),
-                     data=json.dumps(payload),
+      result = requests.post(url, data=json.dumps(payload),
                      headers={'Content-Type': 'application/json',
                               'Accept': 'application/vnd.github.v3.raw+json',
-                              'Authorization': 'token {val}'.format(val=token)})
-      print("The response is sent")
+                              'Authorization': 'token {val}'.format(val=token)}).json()
+      print("The response is sent "+ str(result))
    except requests.exceptions.RequestException:
          print("Error at resource check")
 
