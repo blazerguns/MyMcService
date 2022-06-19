fe06523bb7796837c9d26edaeb09d9652756f06a
blazerguns <varun.chandramohan1@cba.com.au>
2022-06-19 19:18:47 +1000

    Add api info

--- publish_result/publish.py
+++ publish_result/publish.py
@@ -1,9 +1,22 @@
 import sys
+import json
+import requests
 
-def hello():
-   print(sys.argv[1])
-   print(sys.argv[2])
-   print("Hellow") 
+def publish():
+   branch = sys.argv[1]
+   pullid = sys.argv[2]
+   payload = {
+      "body": "First Header | Second Header\n------------ | -------------\nContent from cell 1 | Content from cell 2\nContent in the first column | Content in the second column"
+   }
+   try:
+      result = requests.post(f"https://api.github.com/repos/blazerguns/MyMcService/issues/{id}/comments".format(id=pullid),
+                     data=json.dumps(payload),
+                     headers={'Content-Type': 'application/json',
+                              'Accept': 'application/vnd.github.v3.raw+json',
+                              'Authorization': 'token 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'})
+      print("The response is sent")
+   except requests.exceptions.RequestException:
+         print("Error at resource check")
 
 if __name__ == '__main__':
-    hello()
+    publish()
