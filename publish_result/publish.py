5f52e8e50b6780a60a33eed0f84d51907910ee58
blazerguns <varun.chandramohan1@cba.com.au>
2022-06-20 05:18:16 +1000

    Keys

--- publish_result/publish.py
+++ publish_result/publish.py
@@ -6,7 +6,7 @@ import requests
 def publish():
 
    branch = os.environ.get("BRANCH", "")
-   token = os.environ.get("GHKEYS", "")
+   token = os.environ.get("TOKEN", "")
    print("The token is " + token)
    pullid = "1"
    payload = {
